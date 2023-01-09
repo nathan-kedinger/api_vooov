53124a8e98b85cc07049c665e3839e1ba89982cd
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-01-09 14:38:37 +0100

    Test on generic cruds

--- friends/read.php
+++ friends/read.php
@@ -11,37 +11,42 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
     // Including files for config and data access
     include_once '../../Database.php';
     include_once '../models/CRUD.php';//change with the
+    include_once '../tabs/tabs.php';
 
     // DDB instanciation
     $database = new Database();
     $db = $database->getConnection();
+
+    // crudObject instanciation
+    $crudObject = new CRUD($db);
+
+    // Expected table
     $table = "friends";
 
-    // friend instanciation
-    $friend = new CRUD($db);
+    // Datas
+    $arguments = $tabFriends;// Replace with the good tab
 
     // SQL request
     $sql = "SELECT * FROM " . $table; // It is possible to add a join after that
 
     // Get datas
-    $stmt = $friend->read($sql);
+    $stmt = $crudObject->read($sql);
 
     // Verifying that we have at least one row in database
     if($stmt->rowCount() > 0){
         //initialisation of an associative tab
         $tabFriend = [];
-        $tabFriend['friends'] = [];
+        $tabFriend[$table] = [];
 
         while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
             extract($row);
-
-            $friend = [
-                "uuid" => $uuid,
-                "user1" => $user1 ,
-                "user2" => $user2 ,
-            ];
-
-            $tabFriend['friends'][] = $friend;
+        
+                $friend = [];
+                    foreach($arguments as $argument){
+                        $friend[$argument] = $row[$argument];
+                    }
+        
+            $tabFriend[$table][] = $friend;
         }
 
         http_response_code(200);
