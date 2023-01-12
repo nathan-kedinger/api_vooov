9b4ced3a82f1522f6bf0ac6f27b9d921369019c5
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-01-12 17:04:06 +0100

    Try catch in generic files

--- generic_cruds/generic_create.php
+++ generic_cruds/generic_create.php
@@ -1,48 +1,53 @@
 <?php
+define('ALLOWED_METHOD', 'POST'); // Change with good method
+
 // Headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST"); // Change with good method
+header("Access-Control-Allow-Methods: ALLOWED_METHOD"); 
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-// Verification that used method is correct
-if($_SERVER['REQUEST_METHOD'] == 'POST'){ // Change with good method
-    // Including files for config and data access
-    include_once '../../Database.php';
-    include_once '../models/CRUD.php';
+try{
+    // Verification that used method is correct
+    if($_SERVER['REQUEST_METHOD'] == 'ALLOWED_METHOD'){ // Change with good method
+        throw new Exception("Invalid request method. Only " . ALLOWED_METHOD . " is allowed", 405);
+        // Including files for config and data access
+        include_once '../../Database.php';
+        include_once '../models/CRUD.php';
 
-    // DDB instanciation
-    $database = new Database();
-    $db = $database->getConnection();
+        // DDB instanciation
+        $database = new Database();
+        $db = $database->getConnection();
 
-    // Records instanciation
-    $friend = new CRUD($db);
+        // Records instanciation
+        $crudObject = new CRUD($db);
 
-    // Get back sended informations
-    $datas = json_decode(file_get_contents("php://input"));
+        // Get back sended informations
+        $datas = json_decode(file_get_contents("php://input"));
 
-    foreach($arguments as $argument){
-        if(isset($datas->$argument)){
-            //here we receive datas, we hydrate our object
-            $friend->$argument = $datas->$argument;
+        foreach($arguments as $argument){
+            if(isset($datas->$argument)){
+                //here we receive datas, we hydrate our object
+                $crudObject->$argument = $datas->$argument;
+            }else{
+                // We catch the error
+                http_response_code(400);
+                echo json_encode(["message" => "Arguments doesn't match"]);
+            }
+        }
+        if($crudObject->create($arguments, $sql)){
+            // Here it worked => code 201
+            http_response_code(201);
+            echo json_encode(["message" => "The add have been done"]);
         }else{
-            // We catch the mistake
-            http_response_code(400);
-            echo json_encode(["message" => "Arguments doesn't match"]);
+            // Here it didn't worked => code 503
+            http_response_code(503);
+            echo json_encode(["message" => "The add haven't been done"]);
         }
     }
-    if($friend->create($arguments, $sql)){
-        // Here it worked => code 201
-        http_response_code(201);
-        echo json_encode(["message" => "The add have been done"]);
-    }else{
-        // Here it didn't worked => code 503
-        http_response_code(503);
-        echo json_encode(["message" => "The add haven't been done"]);
-    }
-}else{
-    // We catch the mistake
-    http_response_code(405);
-    echo json_encode(["message" => "This method isn't authorised"]);
+} catch (Exception $e){
+    http_response_code($e->getCode());
+    echo json_encode(["Message" => $e->getMessage()]);
+    error_log($e->getMessage());
 }
