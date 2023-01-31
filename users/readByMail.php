fed26017805bca600decfbc4edf94984e6b81f23
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-01-31 18:04:34 +0100

    create special crud, looking for user by email

---
+++ users/readByMail.php
@@ -0,0 +1,13 @@
+<?php
+    include_once '../tabs/tabs.php';
+
+    // Expected table
+    $table = "users"; // Change with the good BDD table name
+
+    $arguments = $tabUsers;// Replace with the good tab
+
+    $sql = "SELECT ". implode(', ', array_map(function($argument) 
+    { return $argument; }, $arguments)) . " FROM " . $table ."
+    WHERE uuid = ? LIMIT 0,1";
+
+    include_once '../special_cruds/generic_readOneUserByMail.php';
