f92c2b7fc2b760f723e0a3103529f1aa3b8e6e5d
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-02-11 09:04:55 +0100

    log4

--- generic_cruds/generic_read.php
+++ generic_cruds/generic_read.php
@@ -1,12 +1,13 @@
 <?php
+    // Ouvre un fichier pour écrire les journaux d'appel API
+    $logFile = fopen('logs.log', 'a');
 // Headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    // Ouvre un fichier pour écrire les journaux d'appel API
-    $logFile = fopen('logs.log', 'a');
+
     // Écrivez les détails de la requête dans le fichier de journal
     fwrite($logFile, "Method: " . $_SERVER['REQUEST_METHOD'] . "\n");
     fwrite($logFile, "URL: " . $_SERVER['REQUEST_URI'] . "\n");
