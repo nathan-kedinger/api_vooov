e6a46d6983469717f5a807191fd9e0f2eedd35bf
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-02-16 13:42:25 +0100

    Modify readOne message to get all messages of a conversation by the conversation uuid

--- messages/readOne.php
+++ messages/readOne.php
@@ -4,12 +4,12 @@
     // Expected table
     $table = "messages"; // Change with the good BDD table name
 
-    $theOneToGet = "uuid"; // Change with the good column
+    $theOneToGet = "conversation_uuid"; // Change with the good column
 
     $arguments = $tabMessages;// Replace with the good tab
 
     $sql = "SELECT ". implode(', ', array_map(function($argument) 
     { return $argument; }, $arguments)) . " FROM " . $table ."
-    WHERE ". $theOneToGet ." = ? LIMIT 0,1";
+    WHERE ". $theOneToGet ." = ? ";
 
     include_once '../generic_cruds/generic_readOne.php';
