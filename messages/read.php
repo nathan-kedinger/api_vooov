4be8a6ba0b2a4828a3909993a0281091d94cff6e
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-02-16 15:23:20 +0100

    try fixing

--- messages/read.php
+++ messages/read.php
@@ -16,9 +16,9 @@ include_once '../tabs/tabs.php';
     $arguments = $tabMessages;// Replace with the good tab
 
     // SQL request
-    $sql = "SELECT ". implode(', ', array_map(function($argument) 
-    { return $argument; }, $arguments)) . " FROM " . $table ."
-    WHERE ". $theOneToGet ." = ?"; // It is possible to add a join after that
+    $sql = "SELECT " . implode(', ', array_map(function($argument) {
+        return $argument;
+    }, $arguments)) . " FROM " . $table . " WHERE " . $theOneToGet . " = ?"; // It is possible to add a join after that
 
     try{
         /**
